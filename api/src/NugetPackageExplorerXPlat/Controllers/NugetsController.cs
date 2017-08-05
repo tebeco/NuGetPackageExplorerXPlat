14308bd29f0a3d487d35be462cc98075092fa520
TeBeCo <tebeco@gmail.com>
2017-08-05 17:19:28 +0200

    feat(api): target 2.0.0 for netcoreapp and AspNetCore

---
+++ api/src/NugetPackageExplorerXPlat/Controllers/NugetsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NugetPackageExplorerXPlat.Controllers
+{
+    [Route("api/[controller]")]
+    public class NugetsController
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "nuget1", "nuget1" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"nuget {id}";
+        }
+
+    }
+}
