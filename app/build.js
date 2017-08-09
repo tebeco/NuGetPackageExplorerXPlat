e1b17912eaa63cce7a426658d3870125e7ad7de7
TeBeCo <tebeco@gmail.com>
2017-08-09 23:16:01 +0200

    feat(electron-builder): comment build.js to avoid confusion

--- app/build.js
+++ app/build.js
@@ -1,32 +1,34 @@
-const builder = require("electron-builder")
-const Platform = builder.Platform
+// require('ts-node/register');
 
-// Promise is returned 
-builder.build({
-    targets: Platform.WINDOWS.createTarget(),
-    config: {
-        "appId": "cross-platform-desktop",
-        "directories": {
-            "output": "../dist"
-        },
-        "extraResources": {
-            "from": "../api/dist/",
-            "to": "../api/",
-            "filter": [
-                "**/*"
-            ]
-        },
-        "win": {
-            "target": [
-                "NSIS"
-            ]
-        }
-    }
-})
-    .then(() => {
-        console.log('Ok');
-    })
-    .catch((error) => {
-        console.error('Failed :', error);
-        // handle error 
-    })
+// const builder = require("electron-builder")
+// const Platform = builder.Platform
+
+// // Promise is returned 
+// builder.build({
+//     targets: Platform.WINDOWS.createTarget(),
+//     config: {
+//         "appId": "cross-platform-desktop",
+//         "directories": {
+//             "output": "../dist"
+//         },
+//         "extraResources": {
+//             "from": "../api/dist/",
+//             "to": "../api/",
+//             "filter": [
+//                 "**/*"
+//             ]
+//         },
+//         "win": {
+//             "target": [
+//                 "NSIS"
+//             ]
+//         }
+//     }
+// })
+//     .then(() => {
+//         console.log('Ok');
+//     })
+//     .catch((error) => {
+//         console.error('Failed :', error);
+//         // handle error 
+//     })
