952527c3f46ee7f4f0b1bc0f0af27eded020e27b
TeBeCo <tebeco@gmail.com>
2017-08-26 19:07:44 +0200

    WIP: Extract nuget meta

--- api/src/NugetPackageExplorerXPlat/Controllers/NugetsController.cs
+++ api/src/NugetPackageExplorerXPlat/Controllers/NugetsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NuGet.Packaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,21 +9,56 @@ using System.Threading.Tasks;
 namespace NugetPackageExplorerXPlat.Controllers
 {
     [Route("api/[controller]")]
-    public class NugetsController
+    public class NugetsController : Controller
     {
-        // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "nuget1", "nuget1" };
-        }
+            var reader = new PackageArchiveReader("newtonsoft.json.10.0.3.nupkg");
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return $"nuget {id}";
-        }
+            var packageDeps = reader.GetPackageDependencies();
+            var dependencies = packageDeps.ToDictionary(
+                pkgDep => pkgDep.TargetFramework.DotNetFrameworkName,
+                pkgDep => pkgDep.Packages.Select(pkg => new PackageDto() { Name = $"{pkg.Id} {pkg.VersionRange.PrettyPrint()}" }));
 
+            var metadata = reader.NuspecReader.GetMetadata();
+            var extractedMetadata = new
+            {
+                Id = metadata.FirstOrDefault(meta => meta.Key == "id").Value,
+                Version = metadata.FirstOrDefault(meta => meta.Key == "version").Value,
+                Title = metadata.FirstOrDefault(meta => meta.Key == "title").Value,
+                Author = metadata.FirstOrDefault(meta => meta.Key == "authors").Value,
+                Owners = metadata.FirstOrDefault(meta => meta.Key == "owners").Value,
+                MinClientVersion = "WIP",
+                LicenseUrl = metadata.FirstOrDefault(meta => meta.Key == "licenseUrl").Value,
+                ProjectUrl = metadata.FirstOrDefault(meta => meta.Key == "projectUrl").Value,
+                Tags = metadata.FirstOrDefault(meta => meta.Key == "tags").Value,
+                Copyright = metadata.FirstOrDefault(meta => meta.Key == "copyright").Value,
+                Description = metadata.FirstOrDefault(meta => meta.Key == "description").Value,
+                Dependencies = dependencies,
+            };
+
+            return Json(new
+            {
+                Metadata = extractedMetadata,
+
+                PackageContents = new { }
+            },
+            new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            });
+        }
+    }
+
+    public class TargetFrameworkDto
+    {
+        public string Name { get; set; }
+        public IEnumerable<PackageDto> Packages { get; set; }
+    }
+
+    public class PackageDto
+    {
+        public string Name { get; set; }
     }
 }
